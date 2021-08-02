6cbfc9670ef5669cc4a1b48cbdc29b3485c9e7a2
fr4nkastro <85426216+fr4nkastro@users.noreply.github.com>
2021-08-02 00:54:04 -0600

    base codeAdd project files.

---
+++ DataAccess/Repositories/PiezasRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Contracts;
+using DataAccess.Entities;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess.Repositories
+{
+    public class PiezasRepository : MasterRepository, IPiezasRepository
+    {
+        private string selectAll;
+        private string insert;
+        private string update;
+        private string delete;
+
+        public PiezasRepository()
+        {
+            selectAll = "SELECT * FROM PIEZAS;";
+            insert = "INSERT INTO PIEZAS VALUES(@articuloId, @serialno, @precioCosto, @isv, @iva, @descripcion, @modeloId)";
+            update = "UPDATE PIEZAS SET serialno=@serialno, precio_costo=@precioCosto, isv=@isv, iva=@iva, descripcion=@descripcion WHERE articulo_id=@articuloId";
+            delete = "DELETE FROM PIEZAS WHERE articulo_id=@articulo_id";
+        }
+
+        public int Add(Piezas entity)
+        {
+            parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@articuloId", entity.articuloId));
+            parameters.Add(new SqlParameter("@serialno", entity.serialNo));
+            parameters.Add(new SqlParameter("@precioCosto", entity.precioCosto));
+            parameters.Add(new SqlParameter("@isv", entity.isv));
+            parameters.Add(new SqlParameter("@iva", entity.iva));
+            parameters.Add(new SqlParameter("descripcion", entity.descripcion));
+
+            return ExecuteNonQuery(insert);
+        }
+
+        public int Edit(Piezas entity)
+        {
+            parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("@articuloId", entity.articuloId));
+            parameters.Add(new SqlParameter("@serialno", entity.serialNo));
+            parameters.Add(new SqlParameter("@precioCosto", entity.precioCosto));
+            parameters.Add(new SqlParameter("@isv", entity.isv));
+            parameters.Add(new SqlParameter("@iva", entity.iva));
+            parameters.Add(new SqlParameter("descripcion", entity.descripcion));
+
+            return ExecuteNonQuery(update);
+        }
+
+        public IEnumerable<Piezas> GetAll()
+        {
+            var tableResult = ExecuteReader(selectAll);
+            var listPiezas = new List<Piezas>();
+            foreach (DataRow item in tableResult.Rows)
+            {
+                listPiezas.Add(new Piezas
+                {
+                    articuloId= Convert.ToInt32(item[0]),
+                    serialNo=item[1].ToString(),
+                    precioCosto=(float)(item[2]),
+                    isv=(float)(item[3]),
+                    iva=(float)(item[4]),
+                    descripcion=item[5].ToString()
+                    
+                });
+            }
+            return listPiezas;
+        }
+
+        public int Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Repositories/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DataAccess.Repositories
+{
+    public class Repository
+    {
+        private readonly string connectionString;
+        public Repository()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["connInteleSys"].ToString();
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
