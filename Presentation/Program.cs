11a5b34b7c57669100aa3d80140049747b59044e
fr4nkastro <85426216+fr4nkastro@users.noreply.github.com>
2021-08-08 12:29:48 -0600

    Base Code Unstable

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Presentation.Forms;
+
 
 namespace Presentation
 {
@@ -17,7 +17,8 @@ namespace Presentation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Test());
+            Application.Run(new InteleSys.FormLogin());
+
         }
     }
 }
